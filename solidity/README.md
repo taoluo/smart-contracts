83ce0b185b413e4d2169e5d90b3bd1c67343fddc
Tao Luo <tao@Taos-MacBook-Pro.local>
2023-06-05 17:41:38 -0400

    add instruction to run truffle test against geth

--- solidity/README.md
+++ solidity/README.md
@@ -14,3 +14,17 @@ Next, open another terminal, run all tests (requries blockchain running on port 
 truffle test
 ```
 You should see the gas cost for each transaction, and all 21 tests passing.
+
+
+<!-- curl -H 'Content-Type: application/json' --data '{"jsonrpc":"2.0", "id": 1, "method": "debug_traceTransaction", "params": [ "0xc602b44685ac627d3df76a7f68e8b0157d65d0f898b68a89b2c91e24c4e3bb60" ] }' http://127.0.0.1:8545 -->
+
+
+## run test against geth
+docker is needed
+```
+GETH=true ./scripts/geth.sh 
+truffle test
+curl -H 'Content-Type: application/json' --data '{"jsonrpc":"2.0", "id": 1, "method": "debug_traceTransaction", "params": [ "{result.tx/transaction hash}" ] }' http://127.0.0.1:8545
+
+
+```
---
+++ solidity/scripts/geth.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# Set geth version to stable, or pin to a specific version
+# as necessary when geth release breaks CI
+# Snoop these 
+# - https://hub.docker.com/r/ethereum/client-go
+# - https://github.com/ethereum/go-ethereum/releases
+# GETH_VERSION="stable"
+GETH_VERSION="v1.10.26"
+
+docker pull "ethereum/client-go:$GETH_VERSION"
+
+CID=$(docker run \
+  -v /$PWD/scripts:/scripts \
+  -t \
+  -d \
+  -p 8545:8545 \
+  -p 8546:8546 \
+  -p 30303:30303 \
+  "ethereum/client-go:$GETH_VERSION" \
+  --http \
+	--http.api eth,net,web3,debug,txpool \
+  --http.addr '0.0.0.0' \
+  --http.port 8545 \
+  --http.corsdomain '*' \
+  --ws \
+  --ws.addr '0.0.0.0' \
+  --ws.origins '*' \
+  --nodiscover \
+  --dev \
+  --dev.period 0 \
+  --allow-insecure-unlock \
+  --preload ./scripts/geth-accounts.js \
+  console)
+
+printf "Geth running in docker container ${CID}\n"
+printf "Connect to geth:\n  docker exec -it ${CID:0:8} geth attach http://localhost:8545 console\n"
